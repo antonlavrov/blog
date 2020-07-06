9980ad437a7de6ddffae04c35836f06f6649a31a
vagrant <vagrant@homestead>
2020-07-06 17:00:13 +0000

    3aebal

--- resources/views/test/layouts/contact.blade.php
+++ resources/views/test/layouts/contact.blade.php
@@ -1,39 +1,23 @@
 <div class="col-md-9 order-md-1">
    <h4 class="mb-3">Test form</h4>
-<form class="needs-validation" novalidate action="">
-     <div class="row">
-       <div class="col-md-6 mb-3">
-         <label for="firstName">First name</label>
-         <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
-         <div class="invalid-feedback">
-           Valid first name is required.
-         </div>
-       </div>
-       
-       <div class="col-md-6 mb-3">
-         <label for="lastName">Last name</label>
-         <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
-         <div class="invalid-feedback">
-           Valid last name is required.
-         </div>
-       </div>
-     </div>
-
-     <div class="mb-3">
-      <label for="email">Email</label>
-      <input type="email" class="form-control" id="email" placeholder="you@example.com">
-      <div class="invalid-feedback">
-        Please enter a valid email address for shipping updates.
+   <form method="post" action="{{ route('contact') }}">
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" name="name"  placeholder="Jane Doe">
       </div>
+      <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="Email">
       </div>
-
-    <div class="form-group">
-      <label for="exampleFormControlTextarea1">Example textarea</label>
-      <textarea class="form-control" id="textBox" rows="3"></textarea>
-    </div>
-
-    <div class="m-x-auto">
-      <button type="button", class="btn btn-outline-primary">Submit</button>
+      <div class="form-group">
+        <label for="site">Site</label>
+        <input type="text" class="form-control" id="site"  name="site" placeholder="Site">
+      </div>
+      <div class="form-group">
+        <label for="text">Text</label>
+        <textarea class="form-control" id="text" name="text" rows="3"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
     </form>
     </div>
 
