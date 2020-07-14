9e9810412193108cbd31cdee80e1a2f631d6da87
StasLinchenko <55588861+StasLinchenko@users.noreply.github.com>
2020-07-14 13:32:17 +0300

    migrations ans sql

--- storage/framework/views/79c37cd9cd661e1543a110c98fb0e310105ef5ef.php
+++ storage/framework/views/79c37cd9cd661e1543a110c98fb0e310105ef5ef.php
@@ -1,27 +1,4 @@
 <div class="col-md-9 order-md-1">
-<<<<<<< HEAD
-   <h4 class="mb-3">Test form</h4>
-   <form method="post" action="<?php echo e(route('contact')); ?>">
-      <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" class="form-control" id="name" name="name"  placeholder="Jane Doe">
-      </div>
-      <div class="form-group">
-        <label for="email">Email address</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-      </div>
-      <div class="form-group">
-        <label for="site">Site</label>
-        <input type="text" class="form-control" id="site"  name="site" placeholder="Site">
-      </div>
-      <div class="form-group">
-        <label for="text">Text</label>
-        <textarea class="form-control" id="text" name="text" rows="3"></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-    </div>
-=======
     <h4 class="mb-3">Test form</h4>
     <form class="needs-validation" method="POST" action="<?php echo e(route('save-contact')); ?>" novalidate>
         <div class="row">
@@ -49,7 +26,6 @@
                 Please enter a valid email address for shipping updates.
             </div>
         </div>
->>>>>>> eca97a06b798b03c4e9bc71b1538e72f27ccb214
 
         <div class="form-group">
             <label for="exampleFormControlTextarea1">Example textarea</label>
